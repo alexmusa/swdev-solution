dd3d4d4a9a4e8f856f7e40165a3cc5f91d937c4c
Alexandre <alexandre@localhost>
2020-11-22 21:31:25 -0500

    Add TODO comment

--- src/data.cpp
+++ src/data.cpp
@@ -8,6 +8,9 @@ using namespace boost;
 
 Mat imageLoad(filesystem::path const path, cv::ImreadModes const read_mode)
 {
+    // TODO: If the image cannot be read (because of missing file, improper 
+    // permissions, unsupported or invalid format), the function returns an 
+    // empty matrix ( Mat::data==NULL ).
     return imread(path.string(), read_mode);
 }
 
